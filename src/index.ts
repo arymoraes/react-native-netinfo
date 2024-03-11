09e231b92f3b3562b9d6b196235d3a6dec4edf3d
arymoraes <arylmoraesn@gmail.com>
2024-03-11 08:43:18 -0600

    fix: configuration settings not being applied to android

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ export function configure(
     _state = createState();
   }
 
-  if (Platform.OS === 'ios') {
+  if (Platform.OS === 'ios' || Platform.OS === 'android') {
     NativeInterface.configure(configuration);
   }
 }
